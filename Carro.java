8aca845385df7f92b6990d36e6d9aefb36a9e687
Yuri Clay <seu-email@gmail.com>
2026-09-04 11:44:37 -0300

    REVISÃO PROVA AV1:POO Carro

--- Carro.java
+++ Carro.java
@@ -1,28 +1,37 @@
 public class Carro {
-    String modelo;
-    int velocidade;
-    boolean ligado;
 
-    void acelerar(){
-        velocidade =velocidade + 10;
-    }
-    void freiar(){
-      velocidade= velocidade - 10;
-      if(velocidade<0){
-          velocidade = 0;
-      }
-    }
-    void ligar(){
-        ligado =true;
-    }
-    String desligar(){
-        if (velocidade == 0)
-        {
-            ligado = false;
-            return "Carro Desligado";
-        }else{
-            return "Carro Ligado";
-        }
+    private String marca;
+    private String cor;
+    private int ano;
+    private double velocidade;
 
+    public Carro(String marca, String cor, int ano) {
+        this.marca = marca;
+        this.cor = cor;
+        this.ano = ano;
+        this.velocidade = 0;
     }
+    public Carro(String marca, String cor) {
+        this.marca = marca;
+        this.cor = cor;
+        this.ano = 0;
+        this.velocidade = 0;
+    }
+    public void acelerar() {
+        this.velocidade += 10;
+    }
+    public void frear() {
+        this.velocidade -= 10;
+        if (this.velocidade < 0) this.velocidade = 0;
+    }
+    public void pintar(String novaCor) {
+        this.cor = novaCor;
+    }
+    public boolean eseminovo() {
+        return this.ano > 2020;
+    }
+    public String getmarca() { return marca; }
+    public String getcor() { return cor; }
+    public int getano() { return ano; }
+    public double getvelocidade() { return velocidade; }
 }
